ac1c17fa992ef11332cc37415dad1f6180ec3e20
palec87 <david@stanka.de>
2020-06-18 12:48:49 +0100

    take back loading

--- analysis/tests/unit/test_hdf5_import.py
+++ analysis/tests/unit/test_hdf5_import.py
@@ -1,9 +1,9 @@
 import pathlib as p
 import numpy as np
-import cbr_analysis_v0.analysis.data as data
+# import cbr_analysis_v0.analysis.data as data
 # from ...data import data
-# from cbr_analysis_v0.analysis import data
-from cbr_analysis_v0.analysis.experiments.ta import Ta
+from analysis import data
+from analysis.experiments.ta import Ta
 
 
 
